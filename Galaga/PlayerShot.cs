749d1869374208e79691c21bde8994b4add8f14c
LHelding <jonathanhelding@gmail.com>
2023-03-01 15:52:19 +0100

    Fikset collision

--- Galaga/PlayerShot.cs
+++ Galaga/PlayerShot.cs
@@ -10,7 +10,7 @@ public class PlayerShot : Entity {
     public static Vec2F Extent {
         get {return extent;}
         }
-    public PlayerShot(Vec2F position, IBaseImage image) : base(new DynamicShape(position, new Vec2F(0.015f,0.08f)), image) {
+    public PlayerShot(Vec2F position, IBaseImage image) : base(new DynamicShape(position, extent, direction), image) {
     }
 
     public void Move() {
