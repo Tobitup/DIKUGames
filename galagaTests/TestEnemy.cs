3da6c17d97fb93ad1c2ab2def47cc8116fc1a047
LHelding <jonathanhelding@gmail.com>
2023-03-21 16:56:34 +0100

    Tilføhede Testclasses

---
+++ galagaTests/TestEnemy.cs
@@ -0,0 +1,12 @@
+using Library;
+using NUnit.Framework;
+using PersonNS;
+using System;
+
+namespace Tests;
+[TestFixture]
+public class TestEnemy {
+    [SetUp]
+    public void Init() {
+    }
+}
---
+++ galagaTests/TestSquadron.cs
@@ -0,0 +1,12 @@
+using Library;
+using NUnit.Framework;
+using PersonNS;
+using System;
+
+namespace Tests;
+[TestFixture]
+public class TestSquadron {
+    [SetUp]
+    public void Init() {
+    }
+}
