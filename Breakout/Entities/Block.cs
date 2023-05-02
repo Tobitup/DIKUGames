1ec771ca7dddce4d12aea69c2bac6bb439b8248e
theresemira <113703620+theresemira@users.noreply.github.com>
2023-05-02 15:49:06 +0200

    Created Unbreakable

--- Breakout/Entities/Block.cs
+++ Breakout/Entities/Block.cs
@@ -4,7 +4,8 @@ using DIKUArcade.Entities;
 
 namespace Breakout.Blocks;
 
-public class Block : Entity { 
+public class Block : Entity
+{
 
     private int hitpoints;
 
@@ -16,24 +17,28 @@ public class Block : Entity {
     /// <param name="image"> The image to be used for the block. </param>
     /// <return> Returns a Block with a given size, possition and image. </return>
     public Block(Vec2I positionInArray, IBaseImage image) : base(new StationaryShape(
-                new Vec2F(positionInArray.X * 0.083f - 0.04f, positionInArray.Y * 0.041f + 0.40f), 
-                                                            new Vec2F(0.08f, 0.035f)), image) {
+                new Vec2F(positionInArray.X * 0.083f - 0.04f, positionInArray.Y * 0.041f + 0.40f),
+                                                            new Vec2F(0.08f, 0.035f)), image)
+    {
         // Temporary HitPoints for Blocks.
         hitpoints = 123;
         // Temporary Value for Blocks.
         value = 123;
     }
-    
+
     /// <summary> Reduces the hitpoints of a block by 1. </summary>
     /// <return> Void. </return>
-    public void TakeDamage() {
+    public void TakeDamage()
+    {
         hitpoints--;
     }
 
     /// <summary> Checks if a block has 0 or less Hitpoints, in which case it is dead. </summary>
     /// <return> Boolean value to indicate if the block is dead or not. </return>
-    public bool IsDead() {
-        if (hitpoints <= 0) {
+    public bool IsDead()
+    {
+        if (hitpoints <= 0)
+        {
             return true;
         }
         return false;
@@ -41,9 +46,12 @@ public class Block : Entity {
 
     /// <summary> Checks if a block IsDead and if true deletes the entity. </summary>
     /// <return> Void. </return>
-    public void Remove(){
-        if (IsDead()) {
-        DeleteEntity();
+    public void Remove()
+    {
+        if (IsDead())
+        {
+            DeleteEntity();
         }
+
     }
 }
